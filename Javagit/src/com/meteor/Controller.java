33bf46ceaaa9081a16a549e7194dd58b8a33697e
prakash <prakashranjantripathi@gmail.com>
2021-03-10 13:20:22 +0530

    is it working

--- Javagit/src/com/meteor/Controller.java
+++ Javagit/src/com/meteor/Controller.java
@@ -1,5 +1,9 @@
 package com.meteor;
 
+import java.sql.Date;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -8,6 +12,14 @@ public class Controller {
 	public static void main(String[] args) {
 		 List<String> aa = Arrays.asList("hello","hi","how are you?");
          aa.forEach(System.out::println);
+         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+         LocalDateTime now = LocalDateTime.now();
+         java.util.Date time = java.util.Calendar.getInstance().getTime();
+         System.out.println(dtf.format(now));
+         System.out.println(time);
+         
 	}
+
+	
      
 }
